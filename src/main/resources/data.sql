20cc77ba99c92cf00007903341d6cf7420e332d5
Ruszkabányai Ákos <akosruszka@gmail.com>
2018-10-02 01:15:12 +0200

    init sql script added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into blogger(last_name,first_name,age,user_name,email_address,blog_post_counter,reg_date,last_activity_date) values ('Vezeto','Peto',23,'Kilimo','nagyagyu@mukodj.most',0,CURRENT_DATE(),CURRENT_DATE());
+insert into blogger(last_name,first_name,age,user_name,email_address,blog_post_counter,reg_date,last_activity_date) values ('Alamos','Antos',26,'Kimoi','kisagyu@mukodj.most',0,CURRENT_DATE(),CURRENT_DATE());
+insert into blogger(last_name,first_name,age,user_name,email_address,blog_post_counter,reg_date,last_activity_date) values ('Kerek','Perec',99,'Manua','asirba@mukodj.most',0,CURRENT_DATE(),CURRENT_DATE());
+insert into blog_post(content,posted_date,title,author_id) values ('Valami szöveg az elsõ Blogpostnak. Talán lehet egy picit hosszabb is.',CURRENT_DATE(),'Elso BlogPost cím!',select id from blogger where user_name='Kilimo');
+insert into blog_post(content,posted_date,title,author_id) values ('Egy kis szöveg a második BlogPostnak.',CURRENT_DATE(),'Második BlogPost Címe',select id from blogger where user_name='Kimoi');
+insert into blog_post(content,posted_date,title,author_id) values ('Nagyjából valamennyi kitöltõ szöveg a harmadik blogpostnak.',CURRENT_DATE(),'Harmadik címe.',select id from blogger where user_name='Manua');
+insert into blog_post(content,posted_date,title,author_id) values ('Ide is valami kitöltés.',CURRENT_DATE(),'Negyedik Cím.',select id from blogger where user_name='Kilimo');
