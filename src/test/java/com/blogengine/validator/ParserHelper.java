b50ff2f6f15c3486f985cd85b625b4d2f420dc73
Ruszkabányai Ákos <akosruszka@gmail.com>
2018-10-03 15:09:24 +0200

    testEmails to file, TestEmailValidator changed (parameterized test).

---
+++ src/test/java/com/blogengine/validator/ParserHelper.java
@@ -0,0 +1,48 @@
+package com.blogengine.validator;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ParserHelper {
+
+	@SuppressWarnings("rawtypes")
+	public static Collection data() {
+		
+		var resultData = new ArrayList<Object[]>();
+		
+		ClassLoader cl = ParserHelper.class.getClassLoader();
+	
+		String resultString = "";	
+		// JSON to String
+		try {
+			resultString = IOUtils.toString(cl.getResourceAsStream("testEmails.json"));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		JSONParser parser = new JSONParser();	
+		JSONObject rawJSON = null;
+		
+		// JSON (from String) to Object
+		try {
+			rawJSON = (JSONObject) parser.parse(resultString);
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		for(Object key : rawJSON.keySet().toArray()) {
+			JSONObject json = (JSONObject) rawJSON.get(key);
+			resultData.add(new Object[] {json});					
+		}
+	
+		return resultData;			
+	}
+	
+}
